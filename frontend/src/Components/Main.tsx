ee831068a3ab85de8b92c934a5ac49b8eb2f84a7
codeislife1314 <athenaparthenos2@gmail.com>
2021-03-27 18:19:46 -0400

    color fix

--- frontend/src/Components/Main.tsx
+++ frontend/src/Components/Main.tsx
@@ -40,11 +40,11 @@ export default class Main extends React.Component {
                     <SettingModal />
                 </div>
                 <div className="menu">
-                    <button onClick={() => this.setState({ audio: !audio })} className={audio ? "start-stop-btn blue" : "start-stop-btn red"}>
+                    <button onClick={() => this.setState({ audio: !audio })} className={!audio ? "start-stop-btn blue" : "start-stop-btn red"}>
                         <img className="icon1" src={audio ? audioon : audiooff} />
                         {audio ? "Stop " : "Start "} Audio
                     </button>
-                    <button onClick={() => this.setState({ video: !video })} className={video ? "start-stop-btn blue" : "start-stop-btn red"}>
+                    <button onClick={() => this.setState({ video: !video })} className={!video ? "start-stop-btn blue" : "start-stop-btn red"}>
                         <img className="icon2" src={video ? videoon : videooff} />
                         {video ? "Stop " : "Start "} Video
                     </button>
