60166ec9272977179e541770651516191d088439
iiradia <iiradia@ncsu.edu>
2021-03-27 21:52:40 -0400

    Restructured and modified frame processing

--- backend/api/frame_processing.py
+++ backend/api/frame_processing.py
@@ -3,20 +3,47 @@ from api import socketio
 import base64
 import numpy as np
 import cv2
+from imageio import imread
+import io
+from .gaze_tracking import GazeTracking
+
+gaze = GazeTracking()
 
 # Handle the webapp sending a message to the websocket
 @socketio.on('frame')
 def handle_frame(message):
     # print('someone sent to the websocket', message)
     print("message")
-    print('Data', message["data"])
+    #print('Data', message["data"])
     image_string = message["data"]
+        
+    # reconstruct image as an numpy array
     img_data = base64.b64decode(image_string)
-    nparr = np.fromstring(img_data, np.uint8)
-    print("nparr")
-    print(nparr)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    #img = imread(io.BytesIO(base64.b64decode(image_string)))
 
-    print("img_np")
-    print(img_np)
-    print(nparr)
+    nparr = np.fromstring(img_data, np.uint8)
+
+    frame = nparr
+
+    gaze.refresh(frame)
+
+    print(gaze.is_center())
+    is_center = gaze.is_center()
+
+    with open("../ml/frames.txt", "r+") as frames:
+        print(frames)
+        lines = frames.readlines()
+        print(lines[0])
+
+    """
+    if not gaze.is_center():
+        data[a] = true
+        NotificationHelper()
+    """
+
+    
+    #cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    #img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    #print("img_np")
+    #print(cv2_img)
